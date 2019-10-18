fc67e968dae3fc5b0a6579898d2fe6f1717102cc
eqbit <eqbits@gmail.com>
2019-10-18 08:43:54 +0500

    metrics

--- wp-content/themes/cyber/header.php
+++ wp-content/themes/cyber/header.php
@@ -13,6 +13,29 @@
 		rel="stylesheet">
 	<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/app.css">
 	<link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-150335761-1"></script>
+	<script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', 'UA-150335761-1');
+	</script>
+	
+	<script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+      ym(55816039, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });
+	</script>
 </head>
 
 <body>
