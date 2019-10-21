197be806b3fc3add841bbcd2a94865a44011260f
eqbit <eqbits@gmail.com>
2019-10-21 16:58:36 +0500

    popup fix

--- wp-content/themes/cyber/custom/hooks.php
+++ wp-content/themes/cyber/custom/hooks.php
@@ -15,11 +15,11 @@ function form_handle() {
 	$content .= 'Телефон/email: ' .$_POST["phone"]. "\r\n";
 	
 	if($_POST["email"]) {
-		$content .= 'Сообщение: ' .$_POST["email"]. "\r\n";
+		$content .= 'Email: ' .$_POST["email"]. "\r\n";
 	}
 	
 	if($_POST["message"]) {
-		$content .= 'Сообщение: ' .$_POST["message"]. "\r\n";
+		$content .= 'Сообщение: '. "\r\n" .$_POST["message"]. "\r\n";
 	}
 	
 	wp_mail("eqbit@ya.ru", $title, $content);
