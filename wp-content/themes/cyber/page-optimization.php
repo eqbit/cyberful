fb7a8aa88b7bb62871cef79858c4e318e8815785
eqbit <love2eatbraIns>
2019-10-23 22:04:21 +0500

    optimization text edit

--- wp-content/themes/cyber/page-optimization.php
+++ wp-content/themes/cyber/page-optimization.php
@@ -5,26 +5,21 @@
 			<div class="opt-block__inner">
 				<div class="opt-block__img-wrap">
 					<img src="<?= get_template_directory_uri(); ?>/images/opt-img1.svg" alt=""
-					class="opt-block__img">
+					     class="opt-block__img">
 				</div>
 			</div>
 			<div class="opt-block__inner">
 				<h1 class="section-title opt-block__title">
 					Поисковое продвижение сайтов
 				</h1>
-				<div class="styled-redactor">
+				<div class="styled-redactor sub-block-margin">
 					<p>
-						Один из самых сложных, но крайне эффективных видов привлечения посетителей - Search
-						Engine Optimization (SEO), выражаясь доступным языком — поисковая оптимизация.
+						Представьте ситуацию — вы собираетесь что-то найти в интернете, открываете поисковик. Вы
+						современный пользователь и не будете усложнять себе процесс поиска: на выбор
+						предоставляется неограниченное количество ресурсов, но опыт подсказывает, что в первых десяти
+						вы найдете все, что вам требуется.
 					</p>
-					<p>
-						Существует большое количество людей, которые, при наличие такой возможности, не будут кликать
-						на ссылки контекстной рекламы. Для них нахождение сайта в топ 10 поисковой выдачи — признак
-						релевантности запросу. Рекламодатель решает, по каким запросам показать свое объявление. А
-						вот поисковая выдача формируется нейронной сетью, и на самый верх попадают только
-						качественные сайты.
-					</p>
-					<p>Наша стратегия — повышение качества вашего сайта</p>
+					<p>Целью SEO оптимизации является попасть в первую десятку</p>
 				</div>
 			</div>
 		</div>
@@ -39,14 +34,22 @@
 			<div class="opt-clip">
 				<div class="opt-clip__content">
 					<h3 class="section-title opt-clip__title">
-						Зачем это нужно?
+						Зачем это нужно, когда есть контекст?
 					</h3>
-					<p class="opt-clip__text">
-						Обычное дело: вы собираетесь что-то найти в интернете, открываете поисковик. На выбор
-						предоставляется неограниченное количество ресурсов, но опыт подсказывает что в первых десяти
-						вы найдете все, что вам требуется. Современный пользователь не будет усложнять себе процесс
-						поиска. Целью SEO оптимизации является именно попасть в первые ряды.
-					</p>
+					<div class="opt-clip__text">
+						<p>
+							Существует определенный процент людей, которые принципиально не станут кликать
+							на ссылки контекстной рекламы. Они знают: рекламодатель сам решает, по каким поисковым
+							запросам показать свое объявление. Рекламируемый ресурс может решить проблему, но скорее
+							всего это будет пустая потеря времени.
+						</p>
+						<p>
+							С другой строны, нахождение сайта в топ 10 поисковой выдачи — признак
+							релевантности запросу. Поисковая выдача формируется нейронной сетью, и на самый
+							верх попадают только те сайты, которые дают максимально полную информацию по их вопросу.
+							При переходе на такую страницу пользователь подсознательно выдаст ей кредит доверия.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -55,14 +58,18 @@
 	<div class="block-margin"></div>
 	
 	<div class="container">
-		<div class="linear-grid">
+		<h3 class="section-title opt-clip__title">
+			Что мы делаем
+		</h3>
+		
+		<div class="linear-grid sub-block-margin">
 			<div class="linear-card">
 				<img src="<?= get_template_directory_uri(); ?>/images/gc1.svg" alt="seo" class="linear-card__icon">
 				<h4 class="linear-card__title">
-					Глубокая работа с контентом
+					Подбор ключевых слов
 				</h4>
 				<p class="linear-card__text">
-					Создание удобного и привлекательного текста
+					Собираем нужные ключевые фразы в семантическое ядро сайта, сегментируем на кластеры
 				</p>
 			</div>
 			<div class="linear-card">
@@ -71,17 +78,18 @@
 					Эффективная работа сайта
 				</h4>
 				<p class="linear-card__text">
-					Сайт должен работать качественно. Нужно исключить поехавшую верстку на любых разрешениях экрана,
-					ускорить загрузку страниц, оптимизировать изображения
+					Исключаем поехавшую верстку на любых разрешениях экрана,
+					ускоряем загрузку страниц, оптимизируем изображения
 				</p>
 			</div>
 			<div class="linear-card">
 				<img src="<?= get_template_directory_uri(); ?>/images/gc3.svg" alt="seo" class="linear-card__icon">
 				<h4 class="linear-card__title">
-					Подбор ключевых слов
+					Глубокая работа с контентом
 				</h4>
 				<p class="linear-card__text">
-					Собираем нужные ключевые фразы в семантическое ядро сайта, создаем контент под каждую важную фразу
+					Пишем грамотные тексты, хорошо раскрывающие вопрос. Каждая статья будет частью определенного
+					кластера семантического ядра
 				</p>
 			</div>
 			<div class="linear-card">
@@ -90,7 +98,7 @@
 					Качественные ссылки
 				</h4>
 				<p class="linear-card__text">
-					Важны не только качественные внешние ссылки, но и внутренняя перелинковка
+					Важны не только внешние ссылки, но и внутренняя перелинковка
 				</p>
 			</div>
 			<div class="linear-card">
@@ -107,65 +115,72 @@
 			<div class="linear-card">
 				<img src="<?= get_template_directory_uri(); ?>/images/gc6.svg" alt="seo" class="linear-card__icon">
 				<h4 class="linear-card__title">
-					Конверсия
+					Повышение конверсии
 				</h4>
 				<p class="linear-card__text">
 					Нет смысла бесконечно привлекать посетителей на сайт, если они ничего не покупают. С помощью
 					инструментов аналитики мы отслежиаем путь клиента на сайте и даем рекомендации по улучшению
-					конверсионных свойств
+					конверсии
 				</p>
 			</div>
 		
 		</div>
 	</div>
 	
-	<div class="block-margin"></div>
+	<!--<div class="block-margin"></div>
 	
 	<div class="container">
 		<div class="opt-block rev-opt-block">
 			<div class="opt-block__inner rev-opt-block__inner">
 				<div class="opt-block__img-wrap rev-opt-block__img-wrap">
-					<img src="<?= get_template_directory_uri(); ?>/images/opt-img2.svg" alt="pic" class="opt-block__img rev-opt-block__img">
+					<img src="<?= get_template_directory_uri(); ?>/images/opt-img2.svg" alt="pic"
+					     class="opt-block__img rev-opt-block__img">
 				</div>
 			</div>
 			<div class="opt-block__inner rev-opt-block__inner">
 				<h3 class="section-title opt-block__title rev-opt-block__title">
 					Методичность — залог успеха
 				</h3>
-				<p class="opt-block__text rev-opt-block__text">
-					Ввиду того, что поисковые платформы отдают предпочтение только самым качественным страницам, следует
-					постоянно развивать свой сайт. Как только вы прекратите работу, конкуренты тут же обгонят вас
-				</p>
+				<div class="opt-block__text rev-opt-block__text">
+					<p>
+						Ввиду того, что поисковые платформы отдают предпочтение только самым качественным страницам,
+						следует постоянно развивать свой сайт. Нужно периодически добавлять новости, М
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
+	</div>-->
 	
 	<div class="block-margin"></div>
 	
 	<section class="seo-bottom">
 		<!-- this styles are in app-file-->
 		<div class="container-fluid seo-bottom__main-wrap">
-			<img src="<?= get_template_directory_uri(); ?>/images/seo-bottom-bg.png" alt="bg-img" class="seo-bottom__bg-img">
+			<img src="<?= get_template_directory_uri(); ?>/images/seo-bottom-bg.png" alt="bg-img"
+			     class="seo-bottom__bg-img">
 			<span class="seo-bottom__shadow"></span>
 			<div class="container">
 				<h3 class="section-title white-text">
-					Оставьте заявку на сео-аудит вашего сайта
+					Оставьте заявку на сео-аудит сайта
 				</h3>
 				<div class="sub-block-margin"></div>
 				<p class="seo-bottom__text white-text margin-bottom60">
-					Это бесплатно. SEO-специалист Cyberful проведет экспресс-аудит вашего ресурса и свяжется с вами
+					Это бесплатно. SEO-специалист Cyberful проведет экспресс-аудит ресурса, затем мы свяжемся
+					с вами
 				</p>
 				<form class="second-form" data-form-handle>
 					<div class="second-form__side-wrap">
 						<div class="second-form__inputs-wrap">
 							<input type="text" class="second-form__field second-form__input" name="name"
 							       placeholder="Имя" required>
-							<input type="tel" class="second-form__field second-form__input" name="phone" data-phone required
+							<input type="tel" class="second-form__field second-form__input" name="phone" data-phone
+							       required
 							       placeholder="Телефон">
 							<input type="text" class="second-form__field second-form__input" name="site" required
 							       placeholder="Адрес сайта">
 						</div>
-						<textarea name="message" id="#" cols="30" rows="5" class="second-form__field second-form__textarea"
+						<textarea name="message" id="#" cols="30" rows="5"
+						          class="second-form__field second-form__textarea"
 						          placeholder="Кратко опишите ваш проект"></textarea>
 						<input type="hidden" name="form_name" value="Заявка на СЕО аудит сайта">
 						<small class="second-form__privacy">
