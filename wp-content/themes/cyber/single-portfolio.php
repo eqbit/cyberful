eded7f874b578781eabaf4d72c56f75d423fe0cf
eqbit <eqbits@gmail.com>
2019-10-24 16:23:36 +0500

    portfolio

--- wp-content/themes/cyber/single-portfolio.php
+++ wp-content/themes/cyber/single-portfolio.php
@@ -5,6 +5,10 @@
 <div class="container">
 	<h1 class="site-block__title"><? the_title(); ?></h1>
 	
+	<div class="portfolio-info">
+		<? pre(get_the_tags(get_the_id())); ?>
+	</div>
+	
 	<div class="portfolio-content styled-redactor">
 		<?= get_field("main-text"); ?>
 	</div>
