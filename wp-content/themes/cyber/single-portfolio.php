7285426e3fc986485b43c77981b0590ad3cd69a8
eqbit <eqbits@gmail.com>
2019-10-24 16:40:32 +0500

    portfolio

--- wp-content/themes/cyber/single-portfolio.php
+++ wp-content/themes/cyber/single-portfolio.php
@@ -1,20 +1,30 @@
 <? get_header(); ?>
 
-<? the_post(); ?>
+<?
+the_post();
+$post_tags = get_the_terms( get_the_ID(), 'portfolio_tag' );
 
-<div class="container">
-	<h1 class="site-block__title"><? the_title(); ?></h1>
+$fields = get_fields();
+?>
 	
-	<div class="portfolio-info">
-		<?
-		$post_tags = get_the_tags();
-		pre($post_tags);
-		?>
+	<div class="container">
+		<h1 class="site-block__title"><? the_title(); ?></h1>
+		
+		<div class="portfolio-info">
+			<div class="portfolio-info__type">Тип работы: <?= $fields["type"]; ?></div>
+			
+			<? if ( $post_tags && count( $post_tags ) ) : ?>
+				<div class="portfolio-info__tags">
+					<? foreach ( $post_tags as $item ) : ?>
+						<div class="portfolio-info__tag"><?= $item->name; ?></div>
+					<? endforeach; ?>
+				</div>
+			<? endif; ?>
+		</div>
+		
+		<div class="portfolio-content styled-redactor">
+			<?= get_field( "main-text" ); ?>
+		</div>
 	</div>
-	
-	<div class="portfolio-content styled-redactor">
-		<?= get_field("main-text"); ?>
-	</div>
-</div>
 
 <? get_footer();
