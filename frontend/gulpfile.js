ed5e60645c36d25f4ce55fb1aedeb64b540ee8a2
eqbit <eqbits@gmail.com>
2020-01-02 21:50:10 +0500

    added scss to the almost right place

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -62,7 +62,6 @@ const generateHtml = () => {
 
 const optimizeImages = () => {
   return src('src/images/*')
-    // .pipe(imagemin())
     .pipe(dest('build/images'))
     .pipe(browsersync.stream());
 };
@@ -75,10 +74,36 @@ const watcher = () => {
   watch('src/vendor/**', vendor);
 };
 
+const buildStyles = () => {
+  return src('src/scss/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('../wp-content/themes/cyber/css'));
+};
+
+const buildScripts = () => {
+  return src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: [['@babel/preset-env']]
+    }))
+    .pipe(concat('app.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('../wp-content/themes/cyber/js'));
+};
+
+const buildVendor = () => {
+  return src('src/vendor/**')
+    .pipe(dest('../wp-content/themes/cyber/vendor'))
+    .pipe(browsersync.stream());
+};
+
 exports.js = scripts;
 exports.css = styles;
 exports.html = generateHtml;
 exports.images = optimizeImages;
 exports.watch = parallel(watcher, browserSync);
 exports.start = exports.watch;
-exports.build = parallel(scripts, styles, generateHtml, optimizeImages, vendor);
+exports.build = parallel(buildScripts, buildStyles, buildVendor);
+exports.buildLocal = parallel(scripts, styles, generateHtml, optimizeImages, vendor);
