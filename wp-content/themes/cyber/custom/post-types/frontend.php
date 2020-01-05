158ce816128c23d99a2e8e47e2a168af76e874b9
eqbit <eqbits@gmail.com>
2020-01-05 14:36:05 +0500

    fixes

---
+++ wp-content/themes/cyber/custom/post-types/frontend.php
@@ -0,0 +1,41 @@
+<?php
+add_action('init', 'register_post_type_blog');
+function register_post_type_blog(){
+	register_post_type('frontend', array(
+		'labels'             => array(
+			'name'               => 'Статьи', // Основное название типа записи
+			'singular_name'      => 'Статья', // отдельное название записи типа Book
+			'add_new'            => 'Добавить статью',
+			'add_new_item'       => 'Добавить статью',
+			'edit_item'          => 'Редактировать статью',
+			'new_item'           => 'Новая статья',
+			'view_item'          => 'Посмотреть статью',
+			'search_items'       => 'Найти статью',
+			'not_found'          =>  'Статей не найдено',
+			'not_found_in_trash' => 'В корзине статей не найдено',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Frontend'
+		
+		),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array('title','author', 'thumbnail')
+	) );
+}
+
+register_taxonomy('frontend_tag', 'frontend', array(
+		'hierarchical' => false,
+		'label' => "Теги",
+		'singular_name' => "Тег",
+		'rewrite' => true,
+		'query_var' => true
+	)
+);
---
+++ wp-content/themes/cyber/custom/post-types/portfolio.php
@@ -0,0 +1,41 @@
+<?php
+add_action('init', 'register_post_type_portfolio');
+function register_post_type_portfolio(){
+	register_post_type('portfolio', array(
+		'labels'             => array(
+			'name'               => 'Портфолио', // Основное название типа записи
+			'singular_name'      => 'Портфолио', // отдельное название записи типа Book
+			'add_new'            => 'Добавить работу',
+			'add_new_item'       => 'Добавить работу',
+			'edit_item'          => 'Редактировать работу',
+			'new_item'           => 'Новая работа',
+			'view_item'          => 'Посмотреть работу',
+			'search_items'       => 'Найти работу',
+			'not_found'          =>  'Работ не найдено',
+			'not_found_in_trash' => 'В корзине работ не найдено',
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Портфолио'
+		
+		),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => true,
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array('title','author', 'thumbnail')
+	) );
+}
+
+register_taxonomy('portfolio_tag', 'portfolio', array(
+		'hierarchical' => false,
+		'label' => "Теги",
+		'singular_name' => "Тег",
+		'rewrite' => true,
+		'query_var' => true
+	)
+);
