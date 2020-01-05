fea6af675837ed12d9a4f8919215f69b41d11278
eqbit <eqbits@gmail.com>
2020-01-05 16:39:02 +0500

    fixes

--- wp-content/themes/cyber/custom/post-types/frontend.php
+++ wp-content/themes/cyber/custom/post-types/frontend.php
@@ -1,9 +1,9 @@
 <?php
-add_action('init', 'register_post_type_frontend');
-function register_post_type_frontend(){
-	register_post_type('frontend', array(
+add_action('init', 'register_post_type_front');
+function register_post_type_front(){
+	register_post_type('front', array(
 		'labels'             => array(
-			'name'               => 'frontend Статьи',
+			'name'               => 'Front Статьи',
 			'singular_name'      => 'Статья',
 			'add_new'            => 'Добавить статью',
 			'add_new_item'       => 'Добавить статью',
@@ -14,7 +14,7 @@ function register_post_type_frontend(){
 			'not_found'          =>  'Статей не найдено',
 			'not_found_in_trash' => 'В корзине статей не найдено',
 			'parent_item_colon'  => '',
-			'menu_name'          => 'frontendend'
+			'menu_name'          => 'Frontend'
 		
 		),
 		'public'             => true,
@@ -31,7 +31,7 @@ function register_post_type_frontend(){
 	) );
 }
 
-register_taxonomy('frontend_tag', 'frontend', array(
+register_taxonomy('front_tag', 'front', array(
 		'hierarchical' => false,
 		'label' => "Теги",
 		'singular_name' => "Тег",
